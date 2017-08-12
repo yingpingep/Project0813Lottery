a931672b4db1433f5aa49e5d1a97376149a0d6c3
yinnping <ciaoing@livemail.tw>
2017-08-12 23:56:18 +0800

    Add load file function.

---
+++ Project_0813L/DataHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_0813L
+{
+    public class DataHelper
+    {
+        private string[] Names { get; set; }
+        private bool[] Check { get; set; }
+        private Random random;
+
+        public DataHelper()
+        {
+            random = new Random();
+        }
+
+        public bool LoadNames(StreamReader stream)
+        {
+            List<string> names = new List<string>();
+            while (!stream.EndOfStream)
+            {
+                names.Add(stream.ReadLine());
+            }
+            
+            Names = names.ToArray();
+            return true;
+        }
+
+        public string Choose()
+        {            
+            int limit = Names.Length;
+            int index = random.Next(limit);
+
+            while (Check[index])
+            {
+                index = random.Next(limit);
+            }
+
+            return Names[index];
+        }
+    }
+}
--- Project_0813L/MainPage.xaml.cs
+++ Project_0813L/MainPage.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +25,35 @@ namespace Project_0813L
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private DataHelper dataHelper;
+
         public MainPage()
         {
             this.InitializeComponent();
+            dataHelper = new DataHelper();            
+        }
+
+        private void ChooBtn_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private async void LoadBtn_Click(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker picker = new FileOpenPicker();
+            picker.ViewMode = PickerViewMode.List;
+            picker.SuggestedStartLocation = PickerLocationId.Downloads;
+            picker.FileTypeFilter.Add(".txt");
+
+            StorageFile file = await picker.PickSingleFileAsync();
+
+            if (file != null)
+            {                
+                var randoms = await file.OpenReadAsync();                
+                bool isSuccess = dataHelper.LoadNames(new StreamReader(randoms.AsStreamForRead()));
+                if (isSuccess)
+                    NameBlock.Text = "START!!!";
+            }                        
         }
     }
 }
